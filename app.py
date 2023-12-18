dd07d73acf08c19e6d1941e038ede982a0135851
JakeG12345 <jake@glickpeople.com>
2023-12-18 18:48:18 +1100

    connect to open ai api

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-# import os
+import os
 
 # from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 # from flask_session import Session
 # from werkzeug.security import check_password_hash, generate_password_hash
-#TEST IF ASHER CAN COMMIT
-#JACOB
+import openai
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    
+    print("YO 2")
+    
+    print("api key:", os.environ.get("OPENAI_API_KEY"))
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+    response = openai.embeddings.create(
+        model="text-embedding-ada-002",
+        input="The food was delicious and the waiter..."
+    )
+
+    print(response)
+
     return render_template("index.html")
