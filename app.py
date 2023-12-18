6d48e7a7b9143c4d10236a6a1446cb3d41d08a36
JakeG12345 <jake@glickpeople.com>
2023-12-18 21:49:36 +1100

    make quiz page render questions and answers generated by open ai chat gpt api

--- app.py
+++ app.py
@@ -9,16 +9,49 @@ client = OpenAI()
 
 app = Flask(__name__)
 
+# consists of tuples with format (question, answer)
+# quiz_selections = []
+
 @app.route("/")
 def index():
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-    ]
-    )
-
-    print(completion.choices[0].message)
+    # print(completion.choices[0].message)
 
     return render_template("index.html")
+
+@app.route("/quiz")
+def quiz():
+    quiz_selections = []
+
+    if request.method == "POST":
+        selection = request.form.get("option")
+        # global quiz_selections
+        # quiz_selections.append(selection)
+
+    pastSelections = ""
+    for selection in quiz_selections:
+        pastSelections = pastSelections + f" - Question: {selection[0]}, answer: {selection[1]}"
+    if pastSelections != "":
+        pastSelections = "Here are past question and answers: " + pastSelections
+
+    content = ""
+
+    while '::' not in content:
+        data = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": f"Query is coming from an app that helps users come up with ideas through the help of AI. It does this through a series of multiple choice questions that lead to a idea. For question {len(quiz_selections) + 1}, give me a question and a few answers. Remember, question should follow past questions to help lead into an idea for the user. Depending on how far along the question, the more or less specific it should be.{pastSelections}. IMPORTANT: You should return the question, followed by a '::' (without a space inbetween), followed by the possible answers comma separated without a space inbetween. In other words it should look like 'This is the question?::option 1, another option i guess' and you probably will have more options."
+                }
+            ]
+        )
+        content = data.choices[0].message.content
+
+    print("Message:", content)
+    
+    question, optionsD = data.choices[0].message.content.split("::")
+    options = optionsD.split(",")
+    print("Quiz selections:", quiz_selections)
+    print("Options:", options)
+
+    # return render_template("index.html")
+
+    return render_template("quiz.html", question=question, options=options)
