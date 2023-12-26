203efaaa40835779f328313f73d5167a5c704ff6
JakeG12345 <jake@glickpeople.com>
2023-12-26 23:25:51 +1100

    commit serves as message for resolving issues from today regarding jake commits lost

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.secret_key = secrets.token_hex(16)
 
 db = SQL("sqlite:///db.db")
 
-# Bug fix v2
+# Version includes resolved changes from Jake's issue on 26/12/2023
 
 QUESTIONS_BEFORE_IDEAS = 6
 
